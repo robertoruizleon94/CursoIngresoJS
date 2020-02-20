0a0632068cc03aa38031eda65df70f968ff52d87
robertoruizleon94 <60760291+robertoruizleon94@users.noreply.github.com>
2020-02-20 20:49:41 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -1,7 +1,13 @@
 function mostrar()
 {
 
-	var numero = prompt("ingrese un número entre 0 y 10.");
+	var numero = parseInt(prompt("ingrese un número entre 0 y 10."));
+while (numero <0 || numero >9 || isNaN(NUMERO)){
+	numero = parseInt(prompt("Error. Reingrese un número entre 0 y 10."))
+}
+document.getElementById("Numero").value = numero;
+
+
 
 
 }//FIN DE LA FUNCIÓN
