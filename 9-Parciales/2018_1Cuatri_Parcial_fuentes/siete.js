2214190b804c96c5f43e96f0dda825f4eb9f7d53
robertoruizleon94 <60760291+robertoruizleon94@users.noreply.github.com>
2020-02-27 22:14:48 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,37 @@
 function mostrar()
 {
+var nota;
+var sexo;
+var contadorvaronesMas6 = 0;
+var acumuladorNotas = 0;
+var notaBaja;
+var sexoBajo;
+var flag = 0;
+
+for(var i=0; i<5; i++) {
+    
+   nota = parseInt( prompt("ingrese la nota (0-10):"));
+   while( nota <0 || nota >10 || isNaN(nota)) {
+       nota = parseInt(prompt("nota invalida. ingrese nota:"));
+   }
+   sexo = parseInt(prompt("ingrese el sexo (f-m):"));
+ while ( sexo != 'f' && sexo != 'm') {
+     sexo = prompt("sexo invalido. ingrese sexo (f-m):");
+ }
+ 
+ if ( flag == 0 || nota < notaBaja) {
+     notaBaja = nota;
+     sexoBajo = sexo;
+     flag = 1;
+ }
+acumuladorNotas = acumuladorNotas + nota;
+
+if(sexo == 'm' && nota >=6) {
+    contadorvaronesMas6++;
+
+}
+promedio = acumuladorNotas / 5;
+alert("promedio notas: " +promedio+ "")
+}
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,15 @@
 
 function mostrar()
 {
+var ancho;
+var largo;
+var perimetro;
+
+ancho = parseInt(prompt( "ingrese el ancho:" ));
+largo = parseInt( prompt("ingrese el largo:"));
+
+perimetro = ancho * 2 + largo * 2;
+
+alert(perimetro);
 
 }
