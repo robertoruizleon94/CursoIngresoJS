4606bb29ec979b4a6112f30fe15e48bc1f4b4eca
robertoruizleon94 <60760291+robertoruizleon94@users.noreply.github.com>
2020-02-28 22:22:38 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,15 +1,5 @@
 
 function mostrar()
 {
-var ancho;
-var largo;
-var perimetro;
-
-ancho = parseInt(prompt( "ingrese el ancho:" ));
-largo = parseInt( prompt("ingrese el largo:"));
-
-perimetro = ancho * 2 + largo * 2;
-
-alert(perimetro);
 
 }
